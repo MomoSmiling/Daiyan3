a54fd0bb2005398a69ac864f878854b23d84730f
Momo Liu <momoliu@momodembp.attlocal.net>
2018-02-23 01:23:39 -0500

    Add nav bar
    include button intent to another activity.

---
+++ Daiyan/app/src/main/java/com/daiyan/momoliu/daiyan/DaiyanActivity.java
@@ -0,0 +1,51 @@
+package com.daiyan.momoliu.daiyan;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class DaiyanActivity extends AppCompatActivity {
+
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    mTextMessage.setText(R.string.title_home);
+                    return true;
+                case R.id.navigation_message:
+                    mTextMessage.setText(R.string.title_message);
+                    return true;
+                case R.id.navigation_post_a_message:
+                    mTextMessage.setText("Post a message");
+                    return true;
+                case R.id.navigation_search:
+                    mTextMessage.setText(R.string.title_search);
+                    return true;
+                case R.id.navigation_user:
+                    mTextMessage.setText(R.string.title_user);
+                    return true;
+            }
+            return false;
+        }
+
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_daiyan);
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+}
